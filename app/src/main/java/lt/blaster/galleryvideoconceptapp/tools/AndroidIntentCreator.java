8df47e8866819633636c7b885667b3cf6053d8c1
Vidmantas Kerbelis <vkerbelis@yahoo.com>
2016-06-17 16:24:23 +0300

    Add missing intent creator method

--- app/src/main/java/lt/blaster/galleryvideoconceptapp/tools/AndroidIntentCreator.java
+++ app/src/main/java/lt/blaster/galleryvideoconceptapp/tools/AndroidIntentCreator.java
@@ -7,4 +7,13 @@ import android.support.annotation.NonNull;
  * @author Vidmantas Kerbelis (vkerbelis@yahoo.com) on 16.6.17.
  */
 public class AndroidIntentCreator implements IntentCreator {
+
+    @NonNull
+    @Override
+    public Intent createVideoPickerIntent(String title) {
+        Intent intent = new Intent();
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        return Intent.createChooser(intent, title);
+    }
 }
